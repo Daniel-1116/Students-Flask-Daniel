1b89f23c50c4a3aab4a8b9efd3d3aad98362a903
Daniel-1116 <123729963+Daniel-1116@users.noreply.github.com>
2023-02-03 09:20:04 +0200

    commit

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask
-
+from flask import Flask, request, redirect ,url_for ,render_template
+from setup_db import execute_query
 app = Flask(__name__)
 
-@app.route('/route_name')
-def method_name():
-    pass
+@app.route('/')
+def home():
+    return render_template('Registrations.html')
+
+@app.route('/register/<student_id>/<course_id>')
+def register(student_id,course_id):
+    execute_query(f"INSERT INTO students_courses (student_id,course_id) VALUES ('{student_id}','{course_id}')")
+    return redirect(url_for('registrations',student_id=student_id))
+    
+@app.route('/registrations/<student_id>')
+def registrations(student_id):
+    course_ids = [c[0] for c in execute_query(f"SELECT course_id FROM students_courses WHERE student_id={student_id}")] 
+    course_names = [execute_query(f"SELECT name FROM courses WHERE id = {course}") for course in course_ids]  
+    student_name = execute_query(f"SELECT name FROM students WHERE id= '{student_id}'")
+    return render_template('registrations.html',courses = course_names)
