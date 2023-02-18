cdd3aecd987a8db884e1868949129bae4a262ada
Daniel-1116 <123729963+Daniel-1116@users.noreply.github.com>
2023-02-18 17:50:34 +0200

    upgraded search

--- static/index.js
+++ static/index.js
@@ -17,30 +17,31 @@ const courses = document.querySelectorAll('.course');
 });
 
 
-function filterTable() {
-  console.log("Filtering table...");
+// function filterTable() {
+//   console.log("Filtering table...");
   
- 
-  var input = document.getElementById("search-input");
+  const input = document.getElementById("search-input");
+  input.addEventListener('input', () => {
   var keyword = input.value.toLowerCase();
-
 
   var table = document.getElementById("students-table");
   var rows = table.getElementsByTagName("tr");
   for (var i = 0; i < rows.length; i++) {
     var row = rows[i];
-
-    
     var name = row.getElementsByTagName("td")[0];
-    if (name) {
-      var text = name.textContent.toLowerCase();
+    var email = row.getElementsByTagName("td")[1];
+    var course = row.getElementsByTagName("td")[2];
+    if (name && email && course) {
+      var nText = name.textContent.toLowerCase();
+      var eText = email.textContent.toLowerCase();
+      var cText = course.textContent.toLowerCase();
       
 
-      if (text.indexOf(keyword) > -1) {
+      if (nText.indexOf(keyword) > -1 || eText.indexOf(keyword) > -1 || cText.indexOf(keyword) > -1) {
         row.style.display = "";
       } else {
         row.style.display = "none";
       }
     }
   }
-}
+})
